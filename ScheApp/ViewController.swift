3a5ea35c3d1c6105d88d475dcb59fc0bdb58fee9
Admin <admin@Admins-Mac.local>
2019-07-22 01:03:10 +0700

    Initial Commit

---
+++ ScheApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ScheApp
+//
+//  Created by Admin on 31/04/1941 Saka.
+//  Copyright © 1941 Saka Admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
